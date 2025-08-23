a273942fc2aaceea04b49948025b3dd82f7f38e3
poornendra parouha <poornendraparouha@gmail.com>
2025-08-23 09:32:39 +0530

    name changed

---
+++ backend/index.js
@@ -0,0 +1,25 @@
+import express from "express";
+import cors from "cors";
+import todoRoutes from "./todoRoutes.js";
+import noteRoutes from "./noteRoutes.js";
+
+import 'dotenv/config'
+
+const server = express();
+
+// Enable CORS for all routes
+server.use(cors());
+server.use(express.json()); 
+
+// Use your todoRoutes
+server.use("/api/todos", todoRoutes);
+server.use("/api/notes", noteRoutes);
+
+server.get("/", (req, res) => {
+  res.send("Welcome to Utility API");
+});
+
+const PORT = 5000;
+server.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
